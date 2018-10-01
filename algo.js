7e9b74247f1b24b6b8bd1521883b194117a9f346
joisgori <37356528+joisgori@users.noreply.github.com>
2018-10-01 14:22:23 -0600

    Add files via upload

---
+++ algo.js
@@ -0,0 +1,35 @@
+/* función arrow?
+number => number * factor
+*/
+//Investigar sobre CLOSURE -- javascript?
+
+function multiplier(factor) {
+  return number => number * factor;
+}
+
+let twice = multiplier(2);
+console.log(twice(5));
+
+/* imprimiría 10, y multiplier lo puedo definir con el factor que desee y tendrá el espacio de la variables locales reservados en memoria, "atrapados"
+en la variable twice;*/
+
+//Datos atómicos? aquellos que se pueden se parar en partes más pequeñas
+
+
+
+//mutación en javascript
+/*
+ni el igual igual, ni igual igual igual comparan contenido...
+
+
+
+cree un objeto que registre en una lista (array) los eventos de vida de una persona y que tenga
+una propiedad que permita determinar si tuvo o no un accidente ese día.
+
+EventoPer = {
+  Eventos: ['eve1', 'eve2'],
+  accidente: false
+};
+
+¿coeficiente phi?
+*/
